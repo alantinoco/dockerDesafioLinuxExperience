018d6477a64895756d9a2127d1be7b9139a2df1b
Alan <alantinoco@gmail.com>
2022-08-22 14:09:39 -0300

    adicionado model cliente no banco de dados.

--- recepcao/models.py
+++ recepcao/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Visitante(models.Model):
+
+    DESTINO = (
+    ("101", "101"),
+    ("201", "201"),
+    ("301", "301"),
+    ("401", "401"),
+    ("501", "501"),
+)
+
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=50)
+    destino = models.CharField(max_length=3, choices= DESTINO)
+    data = models.DateTimeField(null=True, blank="True")
+
+    def __str__(self):
+        return self.nome
